0119443e57a94fd1abf25db873bfd3d1947125d2
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-08 19:01:56 -0500

    Configure Spring, MVC smoke test

--- src/main/java/com/skilldistillery/mvcsite/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/mvcsite/controllers/FilmController.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.mvcsite.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skilldistillery.mvcsite.data.FilmDAO;
+
+@Controller
 public class FilmController {
 
+	@Autowired
+	private FilmDAO filmDao;
+	
+	@RequestMapping(path = {"/", "home.do"})
+	public String home() {
+		return "WEB-INF/home.jsp";
+	}
+	
+	
 }
