2b37f944c62ebc9a695a6880b29e6fcefad20dfb
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-09 12:05:41 -0500

    removeFilm by Id method complete

--- src/main/java/com/skilldistillery/mvcsite/data/FilmDAOJdbcImpl.java
+++ src/main/java/com/skilldistillery/mvcsite/data/FilmDAOJdbcImpl.java
@@ -12,6 +12,13 @@ import com.skilldistillery.mvcsite.entities.Film;
 
 public class FilmDAOJdbcImpl implements FilmDAO {
 	
+	
+	public static void main(String[] args) {
+		FilmDAOJdbcImpl dao = new FilmDAOJdbcImpl();
+		boolean deleted = dao.deleteFilmById(1009);
+		System.out.println("Deleted: " + deleted);
+	}
+	
 	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false"
 			+ "&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
 	private String user = "student";
@@ -45,9 +52,36 @@ public class FilmDAOJdbcImpl implements FilmDAO {
 	}
 
 	@Override
-	public Film deleteFilmById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean deleteFilmById(int id) {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(URL, user, pass);
+			
+			conn.setAutoCommit(false); // START TRANSACTION
+			
+			String sql = "DELETE FROM film WHERE id = ?";
+			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			int updateCount = stmt.executeUpdate();
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			updateCount = stmt.executeUpdate();
+			
+			conn.commit(); // COMMIT TRANSACTION
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			return false;
+		}
+		return true;
 	}
 
 	@Override
@@ -62,3 +96,6 @@ public class FilmDAOJdbcImpl implements FilmDAO {
 		return null;
 	}
 }
+
+
+
